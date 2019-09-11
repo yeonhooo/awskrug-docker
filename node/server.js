74eb7d5eb849fcae20ec33f59a439dfa2eb16404
YeonHo Lee <yeonhooo@gmail.com>
2019-09-11 15:11:25 +0000

    change container

--- node/server.js
+++ node/server.js
@@ -1,13 +1,19 @@
 'use strict';
+
 const express = require('express');
+
+const os = require('os');
+var hostname = os.hostname();
 
 const PORT = 8000;
 const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world!\n');
+  res.send("<html><head><h1>hostname : " + hostname +
+    	"</h1></head><body><img src='https://blog.docker.com/wp-content/uploads/Swarmnado-357x627-30-1.gif'></body></html>");
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
